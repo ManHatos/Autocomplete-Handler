bfb92afe69ed2df8b47008834f7b8f5b716a33f7
ManHat <88691032+I-ManHat@users.noreply.github.com>
2022-06-20 12:33:20 +0300

    Update and rename ping.js to log.js

---
+++ src/Commands/log.js
@@ -0,0 +1,15 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("log")
+    .setDescription("log a moderation action"),
+    .setDMPermission(false),
+    .addStringOption(option => 
+                     option.setName('user')
+                     .setDescription('The Roblox username of the violator')
+                     .setRequired(true));
+  execute: async (interaction, client) => {
+    return interaction.reply({ content: interaction.data.options[0].value });
+  },
+};
