afb710876991b4604d4083467ea0f62a0b32907c
ManHat <88691032+I-ManHat@users.noreply.github.com>
2022-06-20 13:59:02 +0300

    Update log.js

--- src/Commands/log.js
+++ src/Commands/log.js
@@ -1,30 +1,34 @@
-const { SlashCommandBuilder } = require("@discordjs/builders");
+const {
+    SlashCommandBuilder
+} = require("@discordjs/builders");
 
 module.exports = {
-  data: new SlashCommandBuilder()
-    .setName("log")
-    .setDescription("log a moderation action")
-    .addStringOption(option => 
-                     option.setName('user')
-                     .setDescription('The Roblox username of the violator')
-                     .setRequired(true)
-                     .setAutocomplete(true)),
-  execute: async (interaction, client) => {
-    return interaction.reply({ content: 'Hi', ephemeral: true });
-  },
+    data: new SlashCommandBuilder()
+        .setName("log")
+        .setDescription("log a moderation action")
+        .addStringOption(option =>
+            option.setName('user')
+            .setDescription('The Roblox username of the violator')
+            .setRequired(true)
+            .setAutocomplete(true)),
+    execute: async (interaction, client) => {
+        client.on('interactionCreate', async interaction => {
+            if (!interaction.isAutocomplete()) return;
+            const focusedOption = interaction.options.getFocused(true);
+            let choices;
+
+            if (focusedOption.name === 'user') {
+                choices = ['faq', 'install', 'collection', 'promise', 'debug'];
+            }
+
+            const filtered = choices.filter(choice => choice.startsWith(focusedOption.value));
+            await interaction.respond(
+                filtered.map(choice => ({
+                    name: choice,
+                    value: choice
+                })),
+            );
+        });
+
+    },
 };
-
-client.on('interactionCreate', async interaction => {
-	if (!interaction.isAutocomplete()) return;
-		const focusedOption = interaction.options.getFocused(true);
-		let choices;
-
-		if (focusedOption.name === 'user') {
-			choices = ['faq', 'install', 'collection', 'promise', 'debug'];
-		}
-  
-		const filtered = choices.filter(choice => choice.startsWith(focusedOption.value));
-		await interaction.respond(
-			filtered.map(choice => ({ name: choice, value: choice })),
-		);
-});
