1d18b86b786aefd8ed732884ff524e01a1e8317a
ManHat <88691032+I-ManHat@users.noreply.github.com>
2022-06-20 13:51:02 +0300

    Update log.js

--- src/Commands/log.js
+++ src/Commands/log.js
@@ -13,3 +13,18 @@ module.exports = {
     return interaction.reply({ content: 'Hi', ephemeral: true });
   },
 };
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isAutocomplete()) return;
+		const focusedOption = interaction.options.getFocused(true);
+		let choices;
+
+		if (focusedOption.name === 'user') {
+			choices = ['faq', 'install', 'collection', 'promise', 'debug'];
+		}
+  
+		const filtered = choices.filter(choice => choice.startsWith(focusedOption.value));
+		await interaction.respond(
+			filtered.map(choice => ({ name: choice, value: choice })),
+		);
+});
