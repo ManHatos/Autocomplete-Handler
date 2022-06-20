9168ff891f8cc0753e55fc54ad304618af427b00
ManHat <88691032+I-ManHat@users.noreply.github.com>
2022-06-20 17:16:14 +0300

    Update log.js

--- src/Commands/log.js
+++ src/Commands/log.js
@@ -13,13 +13,16 @@ module.exports = {
         .setAutocomplete(true)
     ),
   execute: async (interaction, client) => {
+    const users = [];
     const response = await axios.get(
       `https://users.roblox.com/v1/users/search?keyword=${interaction.options.data[0].value}&limit=10`
     );
     console.log(response.data);
-
+    response.data.map(match => {
+      users.push(match.displayName + '(' + '@' + match.name + ')');
+    });
     interaction.reply({
-      content: interaction.options.data[0].value + '\n' + JSON.stringify(response.data),
+      content: interaction.options.data[0].value + '\n' + users.join(', '),
       ephemeral: true,
     });
   },
