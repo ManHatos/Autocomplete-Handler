0532f7b6eab92fe86accb9a1d127df043fe94e9b
ManHat <88691032+I-ManHat@users.noreply.github.com>
2022-06-20 12:36:44 +0300

    Update log.js

--- src/Commands/log.js
+++ src/Commands/log.js
@@ -3,8 +3,8 @@ const { SlashCommandBuilder } = require("@discordjs/builders");
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("log")
-    .setDescription("log a moderation action"),
-    .setDMPermission(false),
+    .setDescription("log a moderation action")
+    .setDMPermission(false)
     .addStringOption(option => 
                      option.setName('user')
                      .setDescription('The Roblox username of the violator')
