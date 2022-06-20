c2a5ee0cea241b4f58e31bb8680bbdb1683fd29c
ManHat <88691032+I-ManHat@users.noreply.github.com>
2022-06-20 22:23:50 +0300

    Update log.js

--- src/Commands/log.js
+++ src/Commands/log.js
@@ -1,29 +1,58 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const axios = require('axios');
+const helpers = require('././helpers.js');
 
 module.exports = {
-  data: new SlashCommandBuilder()
-    .setName('log')
-    .setDescription('log a moderation action')
-    .addStringOption(option =>
-      option
-        .setName('user')
-        .setDescription('The Roblox username of the violator')
-        .setRequired(true)
-        .setAutocomplete(true)
-    ),
-  execute: async (interaction, client) => {
-    const users = [];
-    const response = await axios.get(
-      `https://users.roblox.com/v1/users/search?keyword=${interaction.options.data[0].value}&limit=10`
-    );
-    console.log(response.data);
-    response.data.data.map(match => {
-      users.push(match.displayName + '(' + '@' + match.name + ')');
-    });
-    interaction.reply({
-      content: interaction.options.data[0].value + '\n' + users.join(', '),
-      ephemeral: true,
-    });
-  },
+	data: new SlashCommandBuilder()
+		.setName('log')
+		.setDescription('log a moderation action')
+		.addStringOption((option) =>
+			option
+				.setName('user')
+				.setDescription('The Roblox username of the violator')
+				.setRequired(true)
+				.setAutocomplete(true)
+		)
+		.addStringOption((option) =>
+			option
+				.setName('reason')
+				.setDescription('The violations of the violator')
+				.setRequired(true)
+		)
+		.addStringOption((option) =>
+			option
+				.setName('action')
+				.setDescription('The action taken on the violator')
+				.setRequired(true)
+				.addChoices(
+					{
+						name: 'Ban',
+						value: 'Ban',
+					},
+					{
+						name: 'Kick',
+						value: 'Kick',
+					},
+					{
+						name: 'Warning 2',
+						value: 'W2',
+					},
+					{
+						name: 'Warning 1',
+						value: 'W1',
+					},
+					{
+						name: 'Verbal Warning',
+						value: 'VW',
+					}
+				)
+		),
+	execute: async (interaction, client) => {
+		let user = await helpers.user();
+		interaction.reply({
+			content: interaction.options.data[0].value + '\n' + users.join(', '),
+			ephemeral: true,
+		});
+	},
 };
+
