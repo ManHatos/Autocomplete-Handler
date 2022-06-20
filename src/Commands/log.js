877ceb2764ead48b5e7623ba30d4e70a44db4c8b
ManHat <88691032+I-ManHat@users.noreply.github.com>
2022-06-20 14:43:06 +0300

    Update log.js

--- src/Commands/log.js
+++ src/Commands/log.js
@@ -12,23 +12,6 @@ module.exports = {
             .setRequired(true)
             .setAutocomplete(true)),
     execute: async (interaction, client) => {
-        client.on('interactionCreate', async interaction => {
-            if (!interaction.isAutocomplete()) return;
-            const focusedOption = interaction.options.getFocused(true);
-            let choices;
-
-            if (focusedOption.name === 'user') {
-                choices = ['faq', 'install', 'collection', 'promise', 'debug'];
-            }
-
-            const filtered = choices.filter(choice => choice.startsWith(focusedOption.value));
-            await interaction.respond(
-                filtered.map(choice => ({
-                    name: choice,
-                    value: choice
-                })),
-            );
-        });
-
+        interaction.reply({ conten: 'Success!', ephemeral: true });
     },
 };
