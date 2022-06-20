936e1e28b9a96bbfc861f66efe2114b686e0e40b
ManHat <88691032+I-ManHat@users.noreply.github.com>
2022-06-20 16:50:54 +0300

    Update log.js

--- src/Commands/log.js
+++ src/Commands/log.js
@@ -1,17 +1,27 @@
-const {
-    SlashCommandBuilder
-} = require("@discordjs/builders");
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const axios = require('axios');
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName("log")
-        .setDescription("log a moderation action")
-        .addStringOption(option =>
-            option.setName('user')
-            .setDescription('The Roblox username of the violator')
-            .setRequired(true)
-            .setAutocomplete(true)),
-    execute: async (interaction, client) => {
-        interaction.reply({ content: interaction.options.data[0].value, ephemeral: true });
-    },
+  data: new SlashCommandBuilder()
+    .setName('log')
+    .setDescription('log a moderation action')
+    .addStringOption(option =>
+      option
+        .setName('user')
+        .setDescription('The Roblox username of the violator')
+        .setRequired(true)
+        .setAutocomplete(true)
+    ),
+  execute: async (interaction, client) => {
+    try {
+      const response = await axios.get('/user?ID=12345');
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+    interaction.reply({
+      content: interaction.options.data[0].value,
+      ephemeral: true,
+    });
+  },
 };
