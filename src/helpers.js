aceb37f2c60ce38a92c43bd68badfd5190fdf9d6
ManHat <88691032+I-ManHat@users.noreply.github.com>
2022-06-22 11:35:09 +0300

    Update helpers.js

--- src/helpers.js
+++ src/helpers.js
@@ -4,7 +4,7 @@ module.exports = {
 	user: async function user(username) {
 		let userid = username;
 		if (parseInt(username).isNaN) {
-			let userid = await axios.post('https://users.roblox.com/v1/usernames/users', {
+			userid = await axios.post('https://users.roblox.com/v1/usernames/users', {
 				usernames: [username],
 				excludeBannedUsers: false,
 			});
@@ -17,17 +17,21 @@ module.exports = {
 				userid = userAPI.data.data[0].id;
 			}
 		} else {
-			let userAPI = await axios.get('https://users.roblox.com/v1/users/${userid}');
+			let userAPI = await axios.get(
+				'https://users.roblox.com/v1/users/${userid}'
+			);
 			userAPI.data.avatar =
 				'https://cdn.discordapp.com/attachments/893226654086864966/968269659348562011/unknown.png';
-			let avatarAPI = await axios.get(`https://thumbnails.roblox.com/v1/users/avatar?userIds=${userid}&size=720x720&format=Png&isCircular=false`);
+			let avatarAPI = await axios.get(
+				`https://thumbnails.roblox.com/v1/users/avatar?userIds=${userid}&size=720x720&format=Png&isCircular=false`
+			);
 			if (avatarAPI.data.data[0]) {
 				userAPI.data.avatar = avatarAPI.data.data[0].imageUrl;
 			}
 			userAPI.data.created = (
 				new Date(userAPI.data.created).getTime() / 1000
 			).toFixed(0);
-			if (!userAPI.data.description.split('').lenth > 0) {
+			if (userAPI.data.description.split('').lenth <= 0) {
 				userAPI.data.description = null;
 			}
 			return {
@@ -37,5 +41,4 @@ module.exports = {
 		}
 	},
 };
-
 
