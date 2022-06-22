0d2b8beda2cae7c29a7136e89b09fe55407a3f80
ManHat <88691032+I-ManHat@users.noreply.github.com>
2022-06-22 12:11:44 +0300

    Update log.js

--- src/Commands/log.js
+++ src/Commands/log.js
@@ -24,14 +24,7 @@ module.exports = {
 				.setName('action')
 				.setDescription('The action taken on the violator')
 				.setRequired(true)
-				.addChoice(
-					'Ban',
-					'Ban'
-						.addChoice('Kick', 'Kick')
-						.addChoice('Warning 2', 'W2')
-						.addChoice('Warning 1', 'W1')
-						.addChoice('Verbal Warning', 'VW')
-				)
+				.addChoices(['Ban','Ban']);
 		),
 	execute: async (interaction, client) => {
 		let user = await helpers.user(interaction.options.data[0].value);
