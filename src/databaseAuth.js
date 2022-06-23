c9a0c7c383baddbb98b6e27453b5286649bb2f5f
ManHat <88691032+I-ManHat@users.noreply.github.com>
2022-06-23 10:36:29 +0300

    Create databaseAuth.js

---
+++ src/databaseAuth.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "type": "service_account",
+  "project_id": "database-354207",
+  "private_key_id": "484d7d514b24a32f60e2e6616802b3107b9fe9b4",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQCkllGcEUUGRVWo\niMqFjqjxHucCCMhe4hjj+UVBpFB1AYiJbDsHVUtXSZxw0vxynH4KtRVdFLcLZ7rg\nsz1i2DWU3fXzszYtCaqyUm0JUdi+WrR9OGNqVDkN/licAvxWIesEWTIhHobKL6lX\nx2sraO2tVPmuMWbQuDGUWL3C2sR2OqbLReNe2Qk2wGQUutdTafVMvqC66IgfFaWm\n2HhGmOpVtSg/baswktV0tVKouMdgjVdV/ZYEQ2G5mSJBJY41D2koNauwZRzBw8nR\n1bOaoaSeX5ptlsZf2ezcFECrbarPCGqrg+/KEmFChcsUYGRiedGJubTUrIYv2Pxa\nH6FWRRP1AgMBAAECggEACfPZDYHx1ZY9vso+zOK6jpIa0AGt9Cbh8G8zY+vGvbyR\nABZeX2wl+SpHXn8zqs6RxJ38o7tL8lGceFf+X0rJuq3NYKY2oXSTMONRGp4raB9e\nCLBvOo/LP3Bg9VYs4hx89K9cG6qX5gQJzngNn/thW/jmfEUafYaV4O2Y0/6coLWE\nUBGMjHKsAIrNvCJdSvh8cXGwhecWw6uo5RMJNEhksixmTb7BxZperUfiIFZkzwhK\nltmwNu8W1tE+uFK4Qx5+cL3bv+QD78gH+9+COJ3H6hEdKqS1MhPp0GFXZDktFLAs\nBuUxKAvhUJxJoli5e6Re7XRHuxJht17sicAXMzGJ5QKBgQDhKkBaKYfyzVm/P/1d\neG/HFD1Dh2vhdqkIy44I68U1krKRwpre16RTx8cAgQP98GlbGTnWdsFgfY3q/KK2\ntxVStA+gbBrsONxbtRuyvmI2HygRiGYuRAXAYE3LPalgovJmM6eApvA34/76bI+6\naR9vzZoGFZOxPrc8HxQTU+THHwKBgQC7IFou9zlqcHRin2BMfHxA1E5VilTAUZPq\nhR9EDcnCa3lqndQvnEw4L6Pg1jcSFS5H3zWhJte9itjsnutSoBmjxyMa+uwVB4Rq\n0uykSLT3AFh242Oi+ZXHkC2SM+pRaniMDdemi+iEa4lRjRz281rcnFQPKve0+20T\n10QUrUDmawKBgEaEggT6zs33DC1+b+5PIsq5hvpYNiw1injrrz8SAABoQi6uQOyb\noqZugd+hiEuGeKiEPP3y0VtT1xeKKbeJnU76f45hbQkgQbsi/Fq9j2qd1WGwUzgY\nK6j1QEfvttVCEyJ4+POs1LzcVeW+DEF4XNUaEivnpAq1tpNItHPdKWBHAoGAT07m\n6gDd1L7UlPf3WHNqNg/FI3v9b4Ujq8G//pfZaw0h6I374DHeG/IVArXHoWJYux1f\nHW3ZAfxW8/Jmyh4Qv9kmh8jRRhhphtzebsJh8E21MK4vDGO7l1kD93MJXHhDnxg+\nzdd+NKFfEbE9FW0OlXGsLsGHOq1VDZOuxxSm7QECgYBm2Umr69oNMcF3k5aFb/I4\nrDAWo9cjCZe9oHDZMvHxUJbApLgcp9cAdbwFzA4gNnLA4MSUepjIoXsv8SAIoHc4\ndPrUCcKpopMJmoCLjbu/L+v1CycZxGycDcw0hG5i7xpAkDa5BbKB3MI1LWHAhOD5\nlw4X14+9d6STp4ydfL3afQ==\n-----END PRIVATE KEY-----\n",
+  "client_email": "database@database-354207.iam.gserviceaccount.com",
+  "client_id": "111430671042914598240",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/database%40database-354207.iam.gserviceaccount.com"
+}
