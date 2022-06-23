bb9230ba5009d3bfac1a574b964b0690448e56ba
ManHat <88691032+I-ManHat@users.noreply.github.com>
2022-06-23 14:37:08 +0300

    Create export.js

---
+++ src/Commands/export.js
@@ -0,0 +1,23 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const axios = require('axios');
+const helpers = require('../helpers.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('export')
+		.setDescription('Export all total moderation times')
+		.addChannelOption((option) =>
+			option
+				.setName('channel')
+				.setDescription('Channel to send export in')
+				.addChannelTypes([
+					'GuildText',
+					'GuildPublicThread',
+					'GuildPrivateThread',
+				])
+		),
+	execute: async (interaction, client) => {
+		return;
+	},
+};
+
