f169a14af0839bee1bd5b22ecabc39e6886e05ad
ManHat <88691032+I-ManHat@users.noreply.github.com>
2022-06-23 14:24:49 +0300

    Create server.js

---
+++ src/Commands/server.js
@@ -0,0 +1,21 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const axios = require('axios');
+const helpers = require('../helpers.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('server')
+		.setDescription('Announce a server startup, or shutdown')
+		.addSubcommand((subcommand) =>
+			subcommand.setName('startup').setDescription('Announce a server startup')
+		)
+		.addSubcommand((subcommand) =>
+			subcommand
+				.setName('shutdown')
+				.setDescription('Announce a server shutdown')
+		),
+	execute: async (interaction, client) => {
+		return;
+	},
+};
+
