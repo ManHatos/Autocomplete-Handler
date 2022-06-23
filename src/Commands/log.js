5041d0f7ab22f4095b5d800635f553c17dad0bf3
ManHat <88691032+I-ManHat@users.noreply.github.com>
2022-06-23 23:23:50 +0300

    Update log.js

--- src/Commands/log.js
+++ src/Commands/log.js
@@ -33,21 +33,7 @@ module.exports = {
 				)
 		),
 	execute: async (interaction, client) => {
-		let user = await helpers.user(interaction.options.data[0].value);
-		let channel = client.channels.cache.get('914740295961284618');
-		if (user.state == 0) {
-			await interaction.reply({
-				content: `<:Violations:956695827441659915> **The username** \`${interaction.options.getString(
-					'user'
-				)}\` **is incorrect**`,
-				ephemeral: true,
-			});
-			return;
-		}
-		await interaction.reply({
-			content: '**<:Information:956702498809327656> Successfully logged**',
-			ephemeral: true,
-		});
+		return;
 	},
 };
 
