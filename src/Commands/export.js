e264a0c63635d85858c84d60f946fcbe6b25c70a
ManHat <88691032+I-ManHat@users.noreply.github.com>
2022-07-09 01:21:56 +0300

    Update export.js

--- src/Commands/export.js
+++ src/Commands/export.js
@@ -3,16 +3,8 @@ const axios = require('axios');
 const helpers = require('../helpers.js');
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('export')
-		.setDescription('Export all total moderation times')
-		.addChannelOption((option) =>
-			option
-				.setName('channel')
-				.setDescription('Channel to send export in')
-		),
+	data: new SlashCommandBuilder().setName('export').setDescription('Export all total moderation times'),
 	execute: async (interaction, client) => {
 		return;
 	},
 };
-
