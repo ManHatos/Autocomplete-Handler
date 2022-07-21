a537fc881fa3d8b641afb29b74ce0a0d6bd5f3b7
ManHat <88691032+I-ManHat@users.noreply.github.com>
2022-07-21 17:20:50 +0300

    Update bolo.js

--- src/Commands/bolo.js
+++ src/Commands/bolo.js
@@ -1,26 +1,23 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
-const axios = require('axios');
-const helpers = require('../helpers.js');
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const axios = require("axios");
+const helpers = require("../helpers.js");
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('bolo')
-		.setDescription('Put out a ban be-on-the-lookout on a player')
-		.addStringOption((option) =>
-			option
-				.setName('user')
-				.setDescription('The Roblox username of the player')
-				.setRequired(true)
-				.setAutocomplete(true)
-		)
-		.addStringOption((option) =>
-			option
-				.setName('reason')
-				.setDescription('The violations of the player')
-				.setRequired(true)
-		),
-	execute: async (interaction, client) => {
-		return;
-	},
+  data: new SlashCommandBuilder()
+    .setName("bolo")
+    .setDescription("Put out a ban be-on-the-lookout on a player")
+    .addStringOption((option) =>
+      option
+        .setName("user")
+        .setDescription("The Roblox username of the player")
+        .setRequired(true)
+        .setAutocomplete(true)
+        .setMinLength(3)
+    )
+    .addStringOption((option) =>
+      option.setName("reason").setDescription("The violations of the player").setRequired(true).setMaxLength(100)
+    ),
+  execute: async (interaction, client) => {
+    return;
+  },
 };
-
