820246f8f36430f0cd7c0ba21b3340123cb6b200
ManHat <omar.moh.batheeb@gmail.com>
2022-11-06 17:48:25 +0300

    Filter option for /export, replaced d.js builders

--- src/commands/export.js
+++ src/commands/export.js
@@ -1,11 +1,17 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
+const { SlashCommandBuilder } = require('discord.js');
 const axios = require('axios');
 const helpers = require('../helpers.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('export')
-		.setDescription('Export all total moderation times'),
+		.setDescription('Export all total moderation times')
+		.addRoleOption((option) => {
+			option
+				.setName('filter')
+				.setDescription('Export all total moderation times for a role')
+				.setRequired(false)
+		}),
 	execute: async (interaction, client) => {
 		return;
 	},
