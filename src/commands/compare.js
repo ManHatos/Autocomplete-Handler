50ef9b835728d9486933332c814a404999a574bf
ManHat <omar.moh.batheeb@gmail.com>
2022-12-12 16:28:09 +0300

    Added /compare command

---
+++ src/commands/compare.js
@@ -0,0 +1,22 @@
+const { SlashCommandBuilder } = require('discord.js');
+const axios = require('axios');
+const helpers = require('../helpers.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('compare')
+        .setDescription('Compares two total moderation time data files')
+        .setDMPermission(false)
+        .addAttachmentOption((option) =>
+            option
+                .setName('file')
+                .setDescription('The first file to compare')
+        )
+        .addAttachmentOption((option) =>
+            option
+                .setName('file2')
+                .setDescription('The second file to compare')),
+    execute: async (interaction, client) => {
+        return;
+    },
+};
