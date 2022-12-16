378a47c39a52ec859ec3898bb5e1d1a3fb6fc651
ManHat <omar.moh.batheeb@gmail.com>
2022-12-17 00:17:49 +0300

    Updated 'user' autocomplete

--- src/autocomplete/user.js
+++ src/autocomplete/user.js
@@ -3,41 +3,61 @@ const axios = require("axios");
 const helpers = require("../helpers.js");
 
 module.exports = {
-  name: "user",
-  execute: async (interaction, client) => {
-    const focusedOption = interaction.options.getFocused(true);
+	name: "user",
+	execute: async (interaction, client) => {
+		const focusedOption = interaction.options.getFocused(true);
+		let user = focusedOption.value;
 
-    let user = null;
-    if (!focusedOption.name == "user") return;
-    if (!focusedOption.value || focusedOption.value.split("").length < 3)
-      return await interaction.respond([]).catch(async (error) => {
-        console.log(error);
-      });
-    if (focusedOption.value.match(/[\s\S]+ \(@[\s\S]+\)/)) {
-      user = focusedOption.value.match(/(?=[^]+?[^_]+)\w{3,20}(?=\))/)[0];
-    }
-    if (!user) user = focusedOption.value;
+		if (!user || user.length < 3)
+			return await interaction.respond([]).catch(async (error) => {
+				console.error(error);
+			});
+		user = user.match(/[\s\S]+ \(@[\s\S]+\)/) ? user.match(/(?=[^]+?[^_]+)\w{3,20}(?=\))/)[0] || user : user;
 
-    let users = [];
-    let response = await axios.get(`https://users.roblox.com/v1/users/search?keyword=${user}`).catch(function (error) {
-      if (error.response) {
-        console.error("Roblox Search API Error", error.response.data);
-        console.error(error.response.status);
-      } else {
-        console.error("Roblox Search API Error", error.message);
-      }
-    });
-    response?.data?.data?.map((match) => {
-      users.push({
-        name: `${match.displayName.replace(/(?<=.{35})[\s\S]+/, "...")} (@${match.name.replace(
-          /(?<=.{55})[\s\S]+/,
-          "..."
-        )})`,
-        value: match.id.toString(),
-      });
-    });
-    await interaction.respond(users).catch(async (error) => {
-      console.log(error);
-    });
-  },
+		const users = new Array();
+		await axios
+			.get(`https://users.roblox.com/v1/users/search?keyword=${encodeURI(user)}`)
+			.catch(function (error) {
+				if (error.response) {
+					console.error("Roblox Search API Error", error.response.data);
+					console.error(error.response.status);
+				} else {
+					console.error("Roblox Search API Error", error.message);
+				}
+			})
+			.then(function (response) {
+				response?.data?.data?.map((map) => {
+					users.push({
+						name: `${map.displayName.replace(/(?<=.{35})[\s\S]+/, "...")} (@${map.name.replace(/(?<=.{55})[\s\S]+/, "...")})`,
+						value: map.id.toString(),
+					});
+				});
+			});
+		if (!users[0]) {
+			await axios
+				.post(`https://users.roblox.com/v1/usernames/users`, {
+					usernames: [user],
+					excludeBannedUsers: true,
+				})
+				.catch(function (error) {
+					if (error.response) {
+						console.error("Roblox Specific Search API Error", error.response.data);
+						console.error(error.response.status);
+					} else {
+						console.error("Roblox Specific Search API Error", error.message);
+					}
+				})
+				.then(function (response) {
+					response?.data?.data?.map((map) => {
+						users.push({
+							name: `${map.displayName.replace(/(?<=.{35})[\s\S]+/, "...")} (@${map.name.replace(/(?<=.{55})[\s\S]+/, "...")})`,
+							value: map.id.toString(),
+						});
+					});
+				});
+		}
+		await interaction.respond(users).catch(async (error) => {
+			console.error(error);
+		});
+	},
 };
--- src/index.js
+++ src/index.js
@@ -8,77 +8,72 @@ const commandsArray = new Array();
 const { token, guildId } = require("../config.json");
 
 client.once("ready", () => {
-  const commandFiles = fs.readdirSync("src/commands").filter((file) => file.endsWith(".js"));
-  const autocompleteFiles = fs.readdirSync("src/autocomplete").filter((file) => file.endsWith(".js"));
+	const commandFiles = fs.readdirSync("src/commands").filter((file) => file.endsWith(".js"));
+	const autocompleteFiles = fs.readdirSync("src/autocomplete").filter((file) => file.endsWith(".js"));
 
-  for (const file of commandFiles) {
-    const command = require(`./commands/${file}`);
-    map.set(`commands/${command.data.name}`, command);
-    commandsArray.push(command.data.toJSON());
-  }
+	for (const file of commandFiles) {
+		const command = require(`./commands/${file}`);
+		map.set(`commands/${command.data.name}`, command);
+		commandsArray.push(command.data.toJSON());
+	}
 
-  for (const file of autocompleteFiles) {
-    const option = require(`./autocomplete/${file}`);
-    map.set(`autocomplete/${option.name}`, option);
-  }
+	for (const file of autocompleteFiles) {
+		const option = require(`./autocomplete/${file}`);
+		map.set(`autocomplete/${option.name}`, option);
+	}
 
-  const rest = new REST({ version: "10" }).setToken(token);
-  (async () => {
-    try {
-      console.log("Started refreshing application (/) commands.");
+	const rest = new REST({ version: "10" }).setToken(token);
+	(async () => {
+		try {
+			console.log("Started refreshing application (/) commands.");
 
-      await rest.put(Routes.applicationGuildCommands(client.user.id, guildId), {
-        body: commandsArray,
-      });
+			await rest.put(Routes.applicationGuildCommands(client.user.id, guildId), {
+				body: commandsArray,
+			});
 
-      console.log("Successfully reloaded application (/) commands.");
-    } catch (error) {
-      console.error(error);
-    }
-  })();
-  console.log(`Logged in as ${client.user.tag}.`);
+			console.log("Successfully reloaded application (/) commands.");
+		} catch (error) {
+			console.error(error);
+		}
+	})();
+	console.log(`Logged in as ${client.user.tag}.`);
 });
 
 client.on("interactionCreate", async (interaction) => {
-  if (interaction.type === 2) {
-    const command = map.get(`commands/${interaction.commandName}`);
-    if (!command) return;
+	if (interaction.type === 2 /* slash command */) {
+		const command = map.get(`commands/${interaction.commandName}`);
+		if (!command) return;
 
-    try {
-      // await command.execute(interaction, client);
-    } catch (error) {
-      console.error(error);
-      return await interaction.reply({
-        content:
-          "<:FSRP_Warned:961733338329129000> **An unexpected error has occured while trying to execute this command.**\n**Please forward this message to `ManHat#2824`.**\n\n\n```js\n" +
-          error.toJSON() +
-          "```",
-        ephemeral: true,
-      });
-    }
-    console.log(`${interaction.user.tag} (#${interaction.user.id}) executed /${interaction.commandName}`);
-  } else if (interaction.type === 4) {
-    const focusedOption = interaction.options.getFocused(true);
-    const option = map.get(`autocomplete/${focusedOption.name}`);
-    if (!option) return;
+		try {
+			// await command.execute(interaction, client);
+		} catch (error) {
+			console.error(error);
+			return await interaction.reply({
+				content: "<:FSRP_Warned:961733338329129000> **An unexpected error has occured while trying to execute this command.**\n**Please forward this message to `ManHat#2824`.**\n\n\n```js\n" + error.toJSON() + "```",
+				ephemeral: true,
+			});
+		}
+		console.log(`${interaction.user.tag} (#${interaction.user.id}) executed /${interaction.commandName}`);
+	} else if (interaction.type === 4 /* autocomplete */) {
+		const focusedOption = interaction.options.getFocused(true);
+		const option = map.get(`autocomplete/${focusedOption.name}`);
+		if (!option) return;
 
-    try {
-      await option.execute(interaction, client);
-    } catch (error) {
-      console.error(error);
-    }
-    console.log(
-      `${interaction.user.tag} (#${interaction.user.id}) is using '${focusedOption.name}' Autocomplete on /${interaction.commandName}:   ${focusedOption.value}`
-    );
-  }
+		try {
+			await option.execute(interaction, client);
+		} catch (error) {
+			console.error(error);
+		}
+		console.log(`${interaction.user.tag} (#${interaction.user.id}) is using '${focusedOption.name}' Autocomplete on /${interaction.commandName}:   ${focusedOption.value}`);
+	}
 });
 
 // process events
 process.on("uncaughtException", (error) => {
-  console.error(error);
+	console.error(error);
 });
 process.on("warning", (warning) => {
-  console.error(warning.stack);
+	console.error(warning.stack);
 });
 
 client.login(token);
